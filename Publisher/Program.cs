ec848542b01ea188044bcd47dd356989b1fcb82e
olugashi <44696376+olugashi@users.noreply.github.com>
2022-10-17 19:18:02 +0300

    first Commit Initialize

---
+++ Publisher/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace DDSPublisher
+{
+    internal class Program
+    {
+        static readonly int Domain = 45;
+        static readonly int SampleCount = 1000;
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine($"Running HelloWorldPublisher on domain {Domain}");
+            HelloWorldPublisher.RunPublisher(Domain, SampleCount);
+        }
+    }
+}
---
+++ Subscriber/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace DDSSubscriber
+{
+    internal class Program
+    {
+        static readonly int Domain = 45;
+        static readonly int SampleCount = 1000;
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine($"Running HelloWorldSubscriber on domain {Domain}");
+            HelloWorldSubscriber.RunSubscriber(Domain, SampleCount);
+        }
+    }
+}
